a8fb9f06166b6c0b5796381d3a74f2699dfda9a6
Sathyavijayan Vittal <sathyavijayan@gmail.com>
2017-08-31 17:58:52 +0100

    added more components

--- src/kooru/core.clj
+++ src/kooru/core.clj
@@ -107,7 +107,6 @@
 ;;                     ---==| C O M P O N E N T S |==----                     ;;
 ;;                                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (defn many-to-one
   "many-to-one component - reads messages from many channels, calls function f
   with the message, and routes the output messate to a single output channel.
@@ -214,8 +213,75 @@
     ctrl)))
 
 
-;;TODO: one-to-many
-;;TODO: one-to-many-broadcast
+
+(defn one-to-many
+  "one-to-many component - reads messages from a single input channel,
+  calls function f with the message, and routes the output message to
+  one of the output channels based on the response returned by f.
+
+  `id`        - an identifier for this component (used in logs etc).
+  `input-id`  - optional name for the input channel. defaulted to :default
+  `input`     - input channel
+  `f`         - function of arity 2 [state msg].
+                `state`  - persistent state of this component. Set to nil for the
+                           first time.
+                `msg`    - message read from the input channels.
+                f must return a 3-tuple [state' output msg']
+                `state'` - updated state.
+                `output` - name of the output channel to send msg' to.
+                `msg'    - output message
+  `outputs`   - map of output channels keyed by name.
+  `ctrl`      - ctrl provides mechanism necessary to abort incase of an
+                exception and also listen to abort signal from other components
+                in the pipeline this component is a part of."
+  ([id input f outputs ctrl]
+   (one-to-one id :default input f outputs ctrl))
+  ([id input-id input f outputs ctrl]
+   (component*
+    id
+    {input-id input}
+    (fn [state input msg]
+      (let [[state' output msg'] (f state msg)]
+        [state' output msg']))
+    outputs
+    ctrl)))
+
+
+
+(defn one-to-many-broadcast
+  "one-to-many component - reads messages from a single input channel,
+  calls function f with the message, and broadcasts the output message
+  to all the output channels supplied.
+
+  `id`      - an identifier for this component (used in logs etc).
+  `input`   - input channel
+  `f`       - function of arity 2 [state msg].
+              `state`  - persistent state of this component. Set to nil for the
+                         first time.
+              `msg`    - message read from the input channels.
+              f must return a tuple [state' msg']
+              `state'` - updated state.
+              `msg'    - output message
+  `outputs` - map of output channels keyed by name.
+  `ctrl`    - ctrl provides mechanism necessary to abort incase of an
+              exception and also listen to abort signal from other components
+              in the pipeline this component is a part of."
+  [id input f outputs ctrl]
+  ;; the broadcast is setup using an internal output channel which
+  ;; will be passed to the abstract component.
+  (let [internal-output (chan)
+        broadcast       (mult internal-output)]
+    (doall (map (partial tap broadcast) outputs))
+    (component*
+     id
+     {:default input}
+     (fn [state input msg]
+       (let [[state' msg'] (f state msg)]
+         [state' :broadcast msg']))
+     {:broadcast internal-output}
+     ctrl)))
+
+
 
 
 (defn -main
